58184e7bec5a0b6d529ffecaf8b28d0c2f814a49
Avi <avicreationstudio@gmail.com>
2024-08-06 15:48:35 +0530

    connect to db code is added

---
+++ Sample.java
@@ -0,0 +1,13 @@
+class Solution {
+    void logic(int arr[]) {
+        System.out.println(arr[0]);
+    }
+}
+
+public class Sample {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        int matrix[] = { 19, 223, 23, 3242, 342 };
+        obj.logic(matrix);
+    }
+}
